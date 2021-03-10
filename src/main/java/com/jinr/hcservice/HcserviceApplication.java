4da6ce7aa6c29ad908da9344c51e4a3837ec04cb
luojinrui <luojinrui@kingsoft.com>
2021-03-10 20:05:54 +0800

    init code

---
+++ src/main/java/com/jinr/hcservice/HcserviceApplication.java
@@ -0,0 +1,13 @@
+package com.jinr.hcservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HcserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jinr/hcservice/HcserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinr.hcservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
